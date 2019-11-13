d6bb99731c435558801dd98e9c0cfab1615fb83a
KimSweet <101127850+KimSweet@users.noreply.github.com>
2019-11-13 10:08:20 +0000

    Updated Tessssssst and 16 other files

--- blake2-impl.h
+++ blake2-impl.h
@@ -1,4 +1,6 @@
 /*
    BLAKE2 reference source code package - reference C implementations
 
-   Copyright 2012, Samuel Neves <sneves@dei.uc.pt>.  You may
+   Copyright 2012, Samuel Neves <sneves@dei.uc.pt>.  You may use this under the
+   terms of the CC0, the OpenSSL Licence, or the Apache Public License 2.0, at
+   your option.  The terms of these license
