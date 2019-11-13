28746a41a3475165967d175dc40535c8f9cc0175
KimSweet <101127850+KimSweet@users.noreply.github.com>
2019-11-13 18:40:14 +0000

    Update documentation/Server.md

--- blake2-impl.h
+++ blake2-impl.h
@@ -3,4 +3,7 @@
 
    Copyright 2012, Samuel Neves <sneves@dei.uc.pt>.  You may use this under the
    terms of the CC0, the OpenSSL Licence, or the Apache Public License 2.0, at
-   your option.  The terms of these license
+   your option.  The terms of these licenses can be found at:
+
+   - CC0 1.0 Universal : http://creativecommons.org/publicdomain/zero/1.0
+   - OpenSSL license   : https://www.openssl.org/sour
