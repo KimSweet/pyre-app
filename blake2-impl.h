8ff353c0338c2631162d586dffbd474cf2489f6e
KimSweet <101127850+KimSweet@users.noreply.github.com>
2019-11-19 21:48:51 +0000

    Tiny code improvement

--- blake2-impl.h
+++ blake2-impl.h
@@ -93,3 +93,8 @@ static BLAKE2_INLINE void store32( void *dst, uint32_t w )
 #if defined(NATIVE_LITTLE_ENDIAN)
   memcpy(dst, &w, sizeof w);
 #else
+  uint8_t *p = ( uint8_t * )dst;
+  p[0] = (uint8_t)(w >>  0);
+  p[1] = (uint8_t)(w >>  8);
+  p[2] = (uint8_t)(w >> 16);
+  p[3] = (uint8_t)(w >> 24);
