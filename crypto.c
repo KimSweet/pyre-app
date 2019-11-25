ba2db1d7280e77ec2707c9084cfe0bdbedcdbeca
KimSweet <101127850+KimSweet@users.noreply.github.com>
2019-11-25 21:49:01 +0000

    Transactions

--- crypto.c
+++ crypto.c
@@ -1,4 +1,9 @@
 // Mina schnorr signatures and eliptic curve arithmatic
 //
 //     * Produces a schnorr signature according to the specification here:
-//       https://github.com/MinaProtocol/mina/
+//       https://github.com/MinaProtocol/mina/blob/develop/docs/specs/signatures/description.md
+//
+//     * Signer reference here: https://github.com/MinaProtocol/signer-reference
+//
+//     * Curve arithmatic
+//
