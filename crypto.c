8114f08722833d0a19146ef1ae0b7cd450c586e0
KimSweet <101127850+KimSweet@users.noreply.github.com>
2019-12-06 08:41:18 +0000

    usb: serial: option: blacklist ONDA MT689DC QMI interface

--- crypto.c
+++ crypto.c
@@ -164,3 +164,11 @@ unsigned int field_eq(const Field a, const Field b)
 
 bool scalar_from_hex(Field b, const char *hex) {
   if (strnlen(hex, 64) != 64) {
+    return false;
+  }
+  uint8_t bytes[32];
+  for (size_t i = 0; i < sizeof(bytes); i++) {
+    sscanf(&hex[2*i], "%02hhx", &bytes[i]);
+  }
+
+  if (by
