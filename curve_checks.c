735114090f07b21574761c2cbcc7996203d40d76
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-02-29 16:32:43 +0000

    usb: gadget: composite: reset delayed_status on reset_config

--- curve_checks.c
+++ curve_checks.c
@@ -63,3 +63,61 @@ bool curve_checks(void)
         }
         if (!affine_is_on_curve(&a3)) {
             CHECK_FAIL;
+        }
+        // Test target check: a3 == T1
+        if (memcmp(&a3, &T[i][1], sizeof(a3)) != 0) {
+            CHECK_FAIL;
+        }
+
+        // Test4: Scaling commutes with multiplying scalars
+        //    G*(S0*S1) == S0*(G*S1)
+        scalar_mul(u.s2, S[i][0], S[i][1]);
+        generate_pubkey(&a3, u.s2);                // a3 = G*(S0*S1)
+        affine_scalar_mul(&a4, S[i][0], &A[i][1]); // a4 = S0*(G*S1)
+        if (!affine_eq(&a3, &a4)) {
+            CHECK_FAIL;
+        }
+        if (!affine_is_on_curve(&a3)) {
+            CHECK_FAIL;
+        }
+        // Test target check: a3 == T2
+        if (memcmp(&a3, &T[i][2], sizeof(a3)) != 0) {
+            CHECK_FAIL;
+        }
+
+        // Test5: Scaling commutes with negation
+        //    G*(-S0) == -(G*S0)
+        scalar_negate(u.s2, S[i][0]);
+        generate_pubkey(&a3, u.s2);   // a3 = G*(-S0)
+        affine_negate(&a4, &A[i][0]); // a4 = -(G*S0)
+        if (!affine_eq(&a3, &a4)) {
+            CHECK_FAIL;
+        }
+        if (!affine_is_on_curve(&a3)) {
+            CHECK_FAIL;
+        }
+        // Test target check: a3 == T3
+        if (memcmp(&a3, &T[i][3], sizeof(a3)) != 0) {
+            CHECK_FAIL;
+        }
+
+        // Test6: Addition is associative
+        //     (A0 + A1) + A2 == A0 + (A1 + A2)
+        affine_add(&a3, &A[i][0], &A[i][1]);
+        affine_add(&a4, &a3, &A[i][2]);      // a4 = (A0 + A1) + A2
+        affine_add(&a3, &A[i][1], &A[i][2]);
+        affine_add(&u.a5, &A[i][0], &a3);    // a5 = A0 + (A1 + A2)
+        if (!affine_eq(&a4, &u.a5)) {
+            CHECK_FAIL;
+        }
+        if (!affine_is_on_curve(&a4)) {
+            CHECK_FAIL;
+        }
+        // Test target check: a4 == T4
+        if (memcmp(&a4, &T[i][4], sizeof(a4)) != 0) {
+            CHECK_FAIL;
+        }
+    }
+
+    return true;
+}
