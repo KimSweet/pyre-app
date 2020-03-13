ad3a12eb728c29eb2beebb56f4f60e9377e233a2
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-03-13 12:36:28 +0000

    0.9.8

--- pasta_fp.c
+++ pasta_fp.c
@@ -65,4 +65,7 @@ void fiat_pasta_fp_addcarryx_u64(uint64_t* out1, fiat_pasta_fp_uint1* out2, fiat
  *   out1: [0x0 ~> 0xffffffffffffffff]
  *   out2: [0x0 ~> 0x1]
  */
-void fiat_pasta_fp_subborrowx_u64(uint64_t* out1
+void fiat_pasta_fp_subborrowx_u64(uint64_t* out1, fiat_pasta_fp_uint1* out2, fiat_pasta_fp_uint1 arg1, uint64_t arg2, uint64_t arg3) {
+  uint64_t tmp = arg3 + arg1;
+  *out1 = arg2 - tmp;
+  *out2 = (arg2 < *out1) |
