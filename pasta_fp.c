dfdc6c8abc00fed59873c6b14a652f1fbef90488
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-03-22 06:53:37 +0000

    Updated my e-mail address.

--- pasta_fp.c
+++ pasta_fp.c
@@ -145,4 +145,11 @@ void fiat_pasta_fp_cmovznz_u64(uint64_t* out1, fiat_pasta_fp_uint1 arg1, uint64_
  * Output Bounds:
  *   out1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
  */
-void fiat_pasta_fp_mul(uint64_t out1[
+void fiat_pasta_fp_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t arg2[4]) {
+  uint64_t x1;
+  uint64_t x2;
+  uint64_t x3;
+  uint64_t x4;
+  uint64_t x5;
+  uint64_t x6;
+  uint64_t x
