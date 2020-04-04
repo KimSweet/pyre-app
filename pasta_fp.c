cc5d926d9b69cfcc86208f9a9e1ae95cd0897b4e
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-04-04 07:19:42 +0000

    fix the tests

--- pasta_fp.c
+++ pasta_fp.c
@@ -339,4 +339,11 @@ void fiat_pasta_fp_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
   fiat_pasta_fp_uint1 x191;
   uint64_t x192;
   uint64_t x193;
-  uint64_t
+  uint64_t x194;
+  uint64_t x195;
+  x1 = (arg1[1]);
+  x2 = (arg1[2]);
+  x3 = (arg1[3]);
+  x4 = (arg1[0]);
+  fiat_pasta_fp_mulx_u64(&x5, &x6, x4, (arg2[3]));
+  fia
