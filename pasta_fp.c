2e21a36410a29513dc6eba4a873cabfcea04c7bf
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-04-05 20:33:09 +0000

    mm: vmscan: clear kswapd's special reclaim powers before exiting

--- pasta_fp.c
+++ pasta_fp.c
@@ -351,4 +351,7 @@ void fiat_pasta_fp_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
   fiat_pasta_fp_mulx_u64(&x11, &x12, x4, (arg2[0]));
   fiat_pasta_fp_addcarryx_u64(&x13, &x14, 0x0, x12, x9);
   fiat_pasta_fp_addcarryx_u64(&x15, &x16, x14, x10, x7);
-  fiat_pasta_fp_addcarry
+  fiat_pasta_fp_addcarryx_u64(&x17, &x18, x16, x8, x5);
+  x19 = (x18 + x6);
+  fiat_pasta_fp_mulx_u64(&x20, &x21, x11, UINT64_C(0x992d30ecffffffff));
+  fiat_pasta_fp_mulx_u64
