d29ca6a941c403a9776002139e7b3ce736e4f6b0
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-04-18 09:30:39 +0000

    Update phpunit.xml.dist

--- pasta_fp.c
+++ pasta_fp.c
@@ -429,4 +429,6 @@ void fiat_pasta_fp_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
   fiat_pasta_fp_addcarryx_u64(&x158, &x159, x157, x134, x149);
   fiat_pasta_fp_mulx_u64(&x160, &x161, x150, UINT64_C(0x992d30ecffffffff));
   fiat_pasta_fp_mulx_u64(&x162, &x163, x160, UINT64_C(0x4000000000000000));
-  fiat_pasta_fp_mulx_u64(&x164, &x165,
+  fiat_pasta_fp_mulx_u64(&x164, &x165, x160, UINT64_C(0x224698fc094cf91b));
+  fiat_pasta_fp_mulx_u64(&x166, &x167, x160, UINT64_C(0x992d30ed00000001));
+  fiat_pasta_fp_addcarryx_u64(&x168, &x169, 0x0,
