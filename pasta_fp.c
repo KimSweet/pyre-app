24ea4edef1607a1d95883d00e6bfd0f450db1a34
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-04-19 20:43:07 +0000

    Bump maven-javadoc-plugin from 3.1.1 to 3.2.0

--- pasta_fp.c
+++ pasta_fp.c
@@ -437,4 +437,6 @@ void fiat_pasta_fp_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
   fiat_pasta_fp_addcarryx_u64(&x173, &x174, x172, x152, x168);
   fiat_pasta_fp_addcarryx_u64(&x175, &x176, x174, x154, x170);
   fiat_pasta_fp_addcarryx_u64(&x177, &x178, x176, x156, x162);
-  fiat
+  fiat_pasta_fp_addcarryx_u64(&x179, &x180, x178, x158, x163);
+  x181 = ((uint64_t)x180 + x159);
+  fiat_pasta_fp_subborrowx_u64(&x182, &x183, 0x0, x173, UINT64
