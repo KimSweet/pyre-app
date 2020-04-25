c64b7df3b01107d789d84f52de30d84eefee90f5
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-04-25 08:05:24 +0000

    of: fdt: fix memory initialization for expanded DT

--- pasta_fp.c
+++ pasta_fp.c
@@ -483,4 +483,12 @@ void fiat_pasta_fp_square(uint64_t out1[4], const uint64_t arg1[4]) {
   uint64_t x13;
   fiat_pasta_fp_uint1 x14;
   uint64_t x15;
-  fiat_pasta_fp
+  fiat_pasta_fp_uint1 x16;
+  uint64_t x17;
+  fiat_pasta_fp_uint1 x18;
+  uint64_t x19;
+  uint64_t x20;
+  uint64_t x21;
+  uint64_t x22;
+  uint64_t x23;
+  uint64_t x24;
