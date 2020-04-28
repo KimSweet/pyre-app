40bb90670dd71956765be0abe4a4de5e0c524592
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-04-28 12:05:03 +0000

    Bug Fixed

--- pasta_fp.c
+++ pasta_fp.c
@@ -538,4 +538,13 @@ void fiat_pasta_fp_square(uint64_t out1[4], const uint64_t arg1[4]) {
   uint64_t x68;
   uint64_t x69;
   uint64_t x70;
-  uint64_t
+  uint64_t x71;
+  uint64_t x72;
+  uint64_t x73;
+  uint64_t x74;
+  fiat_pasta_fp_uint1 x75;
+  uint64_t x76;
+  uint64_t x77;
+  fiat_pasta_fp_uint1 x78;
+  uint64_t x79;
+  fia
