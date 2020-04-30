38f4e5941360515679527e0e529096392b98b40f
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-04-30 14:27:21 +0000

    1.10.6

--- pasta_fp.c
+++ pasta_fp.c
@@ -586,4 +586,12 @@ void fiat_pasta_fp_square(uint64_t out1[4], const uint64_t arg1[4]) {
   uint64_t x116;
   uint64_t x117;
   uint64_t x118;
-  uint64
+  uint64_t x119;
+  uint64_t x120;
+  uint64_t x121;
+  fiat_pasta_fp_uint1 x122;
+  uint64_t x123;
+  uint64_t x124;
+  fiat_pasta_fp_uint1 x125;
+  uint64_t x126;
+  fiat_pasta_fp_uint1 
