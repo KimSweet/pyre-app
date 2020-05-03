91323f04902a77803132c0d91c5b441525609d27
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-05-03 14:13:19 +0000

    Fix PR feedback

--- pasta_fp.c
+++ pasta_fp.c
@@ -640,4 +640,11 @@ void fiat_pasta_fp_square(uint64_t out1[4], const uint64_t arg1[4]) {
   uint64_t x170;
   uint64_t x171;
   fiat_pasta_fp_uint1 x172;
-  uint64_
+  uint64_t x173;
+  fiat_pasta_fp_uint1 x174;
+  uint64_t x175;
+  fiat_pasta_fp_uint1 x176;
+  uint64_t x177;
+  fiat_pasta_fp_uint1 x178;
+  uint64_t x179;
+  fiat_pasta_fp_uint1 x1
