9822b2652e5387c0a4ca5b7338f7c94cacc26929
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-05-04 17:09:43 +0000

    Incremented revision.

--- pasta_fp.c
+++ pasta_fp.c
@@ -667,4 +667,7 @@ void fiat_pasta_fp_square(uint64_t out1[4], const uint64_t arg1[4]) {
   x2 = (arg1[2]);
   x3 = (arg1[3]);
   x4 = (arg1[0]);
-  fiat_pasta_fp_mulx_u64(&x5, &x6, x4, (arg1[3
+  fiat_pasta_fp_mulx_u64(&x5, &x6, x4, (arg1[3]));
+  fiat_pasta_fp_mulx_u64(&x7, &x8, x4, (arg1[2]));
+  fiat_pasta_fp_mulx_u64(&x9, &x10, x4, (arg1[1]));
+  fiat_pasta_fp_mulx_u64(&x11, &x
