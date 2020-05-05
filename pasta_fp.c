d6def9c02d2377f7879c2bcab5f561c609058769
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-05-05 20:59:45 +0000

    fixing comments

--- pasta_fp.c
+++ pasta_fp.c
@@ -675,4 +675,6 @@ void fiat_pasta_fp_square(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fp_addcarryx_u64(&x15, &x16, x14, x10, x7);
   fiat_pasta_fp_addcarryx_u64(&x17, &x18, x16, x8, x5);
   x19 = (x18 + x6);
-  fiat_pasta_fp_mulx_u64(&x20, &x21, x11, UINT64_C(0x992d30ecffffffff))
+  fiat_pasta_fp_mulx_u64(&x20, &x21, x11, UINT64_C(0x992d30ecffffffff));
+  fiat_pasta_fp_mulx_u64(&x22, &x23, x20, UINT64_C(0x4000000000000000));
+  fiat_pasta_fp_mulx_u64(&x24, &x25, x20, UINT64_C(0x2
