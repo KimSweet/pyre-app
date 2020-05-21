cb58be73b25c009a0752f64ebc5a73bafb72fdc5
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-05-21 09:09:58 +0000

    Корректировка меню в админке

--- pasta_fp.c
+++ pasta_fp.c
@@ -717,4 +717,7 @@ void fiat_pasta_fp_square(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fp_mulx_u64(&x94, &x95, x2, (arg1[0]));
   fiat_pasta_fp_addcarryx_u64(&x96, &x97, 0x0, x95, x92);
   fiat_pasta_fp_addcarryx_u64(&x98, &x99, x97, x93, x90);
-  fiat_pasta_fp_addcarryx_u6
+  fiat_pasta_fp_addcarryx_u64(&x100, &x101, x99, x91, x88);
+  x102 = (x101 + x89);
+  fiat_pasta_fp_addcarryx_u64(&x103, &x104, 0x0, x79, x94);
+  fiat_pasta_fp_addcarryx_u64(&x105, &x106, x10
