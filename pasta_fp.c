486c87c3e02d924e0affc7e76c40e3ac025b4cae
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-05-22 22:09:18 +0000

    Update benchmarks.

--- pasta_fp.c
+++ pasta_fp.c
@@ -741,4 +741,7 @@ void fiat_pasta_fp_square(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fp_mulx_u64(&x139, &x140, x3, (arg1[1]));
   fiat_pasta_fp_mulx_u64(&x141, &x142, x3, (arg1[0]));
   fiat_pasta_fp_addcarryx_u64(&x143, &x144, 0x0, x142, x139);
-  fiat_pasta_fp_
+  fiat_pasta_fp_addcarryx_u64(&x145, &x146, x144, x140, x137);
+  fiat_pasta_fp_addcarryx_u64(&x147, &x148, x146, x138, x135);
+  x149 = (x148 + x136);
+  fiat_pasta_f
