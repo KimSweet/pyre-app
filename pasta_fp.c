673c6d83522b12184f6010d4308741cd0e3d4e5e
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-05-29 20:43:29 +0000

    Correct typos.

--- pasta_fp.c
+++ pasta_fp.c
@@ -786,4 +786,5 @@ void fiat_pasta_fp_square(uint64_t out1[4], const uint64_t arg1[4]) {
  *   0 ≤ eval out1 < m
  *
  * Input Bounds:
- *   arg1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]
+ *   arg1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
+ *   arg2: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xfffffffff
