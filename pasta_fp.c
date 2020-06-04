cd0693e135e9a6d3c959d79a7d41fd4b7fb5aba5
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-06-04 20:50:01 +0000

    Update sphinx from 1.7.5 to 1.7.6

--- pasta_fp.c
+++ pasta_fp.c
@@ -823,4 +823,7 @@ void fiat_pasta_fp_add(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
   fiat_pasta_fp_subborrowx_u64(&x13, &x14, x12, x5, 0x0);
   fiat_pasta_fp_subborrowx_u64(&x15, &x16, x14, x7, UINT64_C(0x4000000000000000));
   fiat_pasta_fp_subborrowx_u64(&x17, &x18, x16, x8, 0x0);
-  fiat_pasta_fp_cmovzn
+  fiat_pasta_fp_cmovznz_u64(&x19, x18, x9, x1);
+  fiat_pasta_fp_cmovznz_u64(&x20, x18, x11, x3);
+  fiat_pasta_fp_cmovznz_u64(&x21, x18, x13, x5);
+  fiat_pasta_fp_
