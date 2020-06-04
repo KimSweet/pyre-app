cb7074c76fa31eb804a6f5d314a7314a97b8965e
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-06-04 14:24:29 +0000

    Hmm...

--- pasta_fp.c
+++ pasta_fp.c
@@ -814,4 +814,6 @@ void fiat_pasta_fp_add(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
   uint64_t x20;
   uint64_t x21;
   uint64_t x22;
-  fiat_pasta_fp_addcarryx_u64(&x1, &x2, 0x0, (arg1[0]),
+  fiat_pasta_fp_addcarryx_u64(&x1, &x2, 0x0, (arg1[0]), (arg2[0]));
+  fiat_pasta_fp_addcarryx_u64(&x3, &x4, x2, (arg1[1]), (arg2[1]));
+  fiat_pasta_fp_addcarryx_u64(&x5, &x6, x4, (arg1[2]), (arg2
