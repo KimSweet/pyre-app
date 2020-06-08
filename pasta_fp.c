dcae381f4b956c458c335fc759728501b2bcebdb
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-06-08 15:41:22 +0000

    New results

--- pasta_fp.c
+++ pasta_fp.c
@@ -865,4 +865,6 @@ void fiat_pasta_fp_sub(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
   uint64_t x14;
   fiat_pasta_fp_uint1 x15;
   uint64_t x16;
-  fiat_pas
+  fiat_pasta_fp_uint1 x17;
+  fiat_pasta_fp_subborrowx_u64(&x1, &x2, 0x0, (arg1[0]), (arg2[0]));
+  fiat_pasta_fp_subborrowx_u64(&x3, &x4, x2, (arg1[1]), 
