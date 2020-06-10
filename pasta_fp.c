0b35ae52808173811ed0be7fca2d3f2bd9c0629f
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-06-10 11:36:37 +0000

    Adds unit tests

--- pasta_fp.c
+++ pasta_fp.c
@@ -908,4 +908,9 @@ void fiat_pasta_fp_opp(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fp_uint1 x11;
   uint64_t x12;
   fiat_pasta_fp_uint1 x13;
-  uint64_t x1
+  uint64_t x14;
+  fiat_pasta_fp_uint1 x15;
+  uint64_t x16;
+  fiat_pasta_fp_uint1 x17;
+  fiat_pasta_fp_subborrowx_u64(&x1, &x2, 0x0, 0x0, (arg1[0]));
+  fiat_pasta_fp_s
