0443d8381a932d85043622c2a6f1448a3c40cf33
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-06-11 20:28:11 +0000

    swap: avoid read_swap_cache_async() race to deadlock while waiting on discard I/O completion

--- pasta_fp.c
+++ pasta_fp.c
@@ -915,4 +915,6 @@ void fiat_pasta_fp_opp(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fp_subborrowx_u64(&x1, &x2, 0x0, 0x0, (arg1[0]));
   fiat_pasta_fp_subborrowx_u64(&x3, &x4, x2, 0x0, (arg1[1]));
   fiat_pasta_fp_subborrowx_u64(&x5, &x6, x4, 0x0, (arg1[2]));
-  fiat_pasta_fp_subborrowx_u64(&x7, &x8
+  fiat_pasta_fp_subborrowx_u64(&x7, &x8, x6, 0x0, (arg1[3]));
+  fiat_pasta_fp_cmovznz_u64(&x9, x8, 0x0, UINT64_C(0xffffffffffffffff));
+  fiat_pasta_fp_addcarryx_u64(&x10, &x11,
