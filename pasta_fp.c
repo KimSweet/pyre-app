3cc93e9c82c0a37adb58c991c24fd27f3b232e82
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-06-17 07:25:21 +0000

    Fixed namespace

--- pasta_fp.c
+++ pasta_fp.c
@@ -936,4 +936,6 @@ void fiat_pasta_fp_opp(uint64_t out1[4], const uint64_t arg1[4]) {
  *   0 ≤ eval out1 < m
  *
  * Input Bounds:
- *   arg1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~
+ *   arg1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
+ * Output Bounds:
+ *   out1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]
