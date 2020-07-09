37003cca188e0f4cc5763de39561e21febd60df9
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-07-09 21:04:23 +0000

    Update mainwindow.cpp

--- pasta_fp.c
+++ pasta_fp.c
@@ -1050,4 +1050,7 @@ void fiat_pasta_fp_from_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fp_addcarryx_u64(&x16, &x17, 0x0, x14, (arg1[1]));
   fiat_pasta_fp_mulx_u64(&x18, &x19, x16, UINT64_C(0x992d30ecffffffff));
   fiat_pasta_fp_mulx_u64(&x20, &x21, x18, UINT64_C(0x4000000000000000));
-  fiat_pasta_fp_mulx_u64(&x22, &x23, x18, UINT64_C(0x224698fc094cf91b))
+  fiat_pasta_fp_mulx_u64(&x22, &x23, x18, UINT64_C(0x224698fc094cf91b));
+  fiat_pasta_fp_mulx_u64(&x24, &x25, x18, UINT64_C(0x992d30ed00000001));
+  fiat_pasta_fp_addcarryx_u64(&x26, &x27, 0x0, x25, x22);
+  fiat_pasta_fp_addcarryx_u64
