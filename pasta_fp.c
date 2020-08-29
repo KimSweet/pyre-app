642fcc7c9cd943f3ce740674eab0821f4d1fcbdc
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-08-29 16:17:33 +0000

    workqueue: use mutex for global_cwq manager exclusion

--- pasta_fp.c
+++ pasta_fp.c
@@ -1106,4 +1106,6 @@ void fiat_pasta_fp_from_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
  *   0 ≤ eval out1 < m
  *
  * Input Bounds:
- *   arg1: [[0x0 ~> 0xffffffffffffffff], [0
+ *   arg1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
+ * Output Bounds:
+ *   out1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xfffffffff
