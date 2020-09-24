43ba4481eaa4a393a8ff79f1664d8078ff1824bc
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-09-24 21:32:49 +0000

    Update slip.blade.php

--- pasta_fp.c
+++ pasta_fp.c
@@ -1290,4 +1290,6 @@ void fiat_pasta_fp_to_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fp_mulx_u64(&x7, &x8, x4, UINT64_C(0x7797a99bc3c95d18));
   fiat_pasta_fp_mulx_u64(&x9, &x10, x4, UINT64_C(0xd7d30dbd8b0de0e7));
   fiat_pasta_fp_mulx_u64(&x11, &x12, x4, UINT64_C(0x8c78ecb30000000f));
-  
+  fiat_pasta_fp_addcarryx_u64(&x13, &x14, 0x0, x12, x9);
+  fiat_pasta_fp_addcarryx_u64(&x15, &x16, x14, x10, x7);
+  fiat_pasta_fp_addcarryx_u64(&x17, &x18
