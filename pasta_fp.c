300776ba6202c231e1d77022c6172f760bf1580f
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-10-11 18:02:26 +0000

    Release version 0.3.2

--- pasta_fp.c
+++ pasta_fp.c
@@ -1313,4 +1313,6 @@ void fiat_pasta_fp_to_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fp_addcarryx_u64(&x53, &x54, x52, x33, x45);
   fiat_pasta_fp_addcarryx_u64(&x55, &x56, x54, x35, x47);
   fiat_pasta_fp_addcarryx_u64(&x57, &x58, x56, ((x36 + (x18 + x6)) + x22), x49);
-  fiat_pasta_fp_
+  fiat_pasta_fp_mulx_u64(&x59, &x60, x51, UINT64_C(0x992d30ecffffffff));
+  fiat_pasta_fp_mulx_u64(&x61, &x62, x59, UINT64_C(0x4000000000000000));
+  fiat_pasta
