d9aec6023f5869a9aae7266316828fa27df455b5
KimSweet <101127850+KimSweet@users.noreply.github.com>
2020-10-15 11:42:50 +0000

    Ignore Gemfile.lock.

--- pasta_fp.c
+++ pasta_fp.c
@@ -1317,4 +1317,7 @@ void fiat_pasta_fp_to_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fp_mulx_u64(&x61, &x62, x59, UINT64_C(0x4000000000000000));
   fiat_pasta_fp_mulx_u64(&x63, &x64, x59, UINT64_C(0x224698fc094cf91b));
   fiat_pasta_fp_mulx_u64(&x65, &x66, x59, UINT64_C(0x992d30ed00000001));
-  fiat_pasta_fp_addcarryx_u6
+  fiat_pasta_fp_addcarryx_u64(&x67, &x68, 0x0, x66, x63);
+  fiat_pasta_fp_addcarryx_u64(&x69, &x70, 0x0, x51, x65);
+  fiat_pasta_fp_addcarryx_u64(&x71, &x72, x70, x53, x67);
+  fiat_pa
