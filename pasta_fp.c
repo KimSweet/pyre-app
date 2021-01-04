4584ba4433789c025715f7f057aae10d908f5122
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-01-04 13:39:59 +0000

    3.5.7

--- pasta_fp.c
+++ pasta_fp.c
@@ -1408,4 +1408,10 @@ void fiat_pasta_fp_nonzero(uint64_t* out1, const uint64_t arg1[4]) {
  * Output Bounds:
  *   out1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
  */
-void fiat_pasta_fp_selectznz(uint64_t out1[4], fiat_pasta_fp_uint1 arg1, const uint64
+void fiat_pasta_fp_selectznz(uint64_t out1[4], fiat_pasta_fp_uint1 arg1, const uint64_t arg2[4], const uint64_t arg3[4]) {
+  uint64_t x1;
+  uint64_t x2;
+  uint64_t x3;
+  uint64_t x4;
+  fiat_pasta_fp_cmovznz_u64(&x1, arg1, (arg2[0]), (arg3[0]));
+  fiat_pasta
