6dfc4c9ff9867441546b586dbdb4a0e4fb3b94c5
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-07-27 13:13:59 +0000

    Version bump to 0.8.3

--- pasta_fp.c
+++ pasta_fp.c
@@ -1790,4 +1790,4 @@ void fiat_pasta_fp_divstep_precomp(uint64_t out1[4]) {
  * Postconditions:
  *   out1 = (if 0 < arg1 ∧ (twos_complement_eval arg3) is odd then 1 - arg1 else 1 + arg1)
  *   twos_complement_eval out2 = (if 0 < arg1 ∧ (twos_complement_eval arg3) is odd then twos_complement_eval arg3 else twos_complement_eval arg2)
- *   twos_co
+ *   twos_complement_eval out3 = (if 0 < arg1 ∧ (twos_complement_eval arg3) is odd then ⌊(twos_complement_eval arg3 - twos_complement_eval arg2) / 2⌋ else ⌊(t
