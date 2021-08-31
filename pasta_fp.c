835a32aba4866f08ad0e9c6a489ec274d6b5ce18
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-08-31 22:36:25 +0000

    better defaults

--- pasta_fp.c
+++ pasta_fp.c
@@ -1980,4 +1980,6 @@ void fiat_pasta_fp_divstep(uint64_t* out1, uint64_t out2[5], uint64_t out3[5], u
   fiat_pasta_fp_subborrowx_u64(&x59, &x60, x58, 0x0, x49);
   fiat_pasta_fp_cmovznz_u64(&x61, x60, 0x0, UINT64_C(0xffffffffffffffff));
   fiat_pasta_fp_addcarryx_u64(&x62, &x63, 0x0, x53, (x61 & UINT64_C(0x992d30ed00000001)));
-  fiat_pasta_fp_addcarryx_u64
+  fiat_pasta_fp_addcarryx_u64(&x64, &x65, x63, x55, (x61 & UINT64_C(0x224698fc094cf91b)));
+  fiat_pasta_fp_addcarryx_u64(&x66, &x67, x65, x57, 0x0);
+  fiat_
