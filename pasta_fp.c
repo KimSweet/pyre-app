3ea7807757e31d8fdb6dcbc6e4f5d55c94135b8c
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-09-06 21:08:13 +0000

    Update custom-commands.js

--- pasta_fp.c
+++ pasta_fp.c
@@ -2014,4 +2014,6 @@ void fiat_pasta_fp_divstep(uint64_t* out1, uint64_t out2[5], uint64_t out3[5], u
   fiat_pasta_fp_addcarryx_u64(&x112, &x113, 0x0, x6, 0x1);
   x114 = ((x80 >> 1) | ((x82 << 63) & UINT64_C(0xffffffffffffffff)));
   x115 = ((x82 >> 1) | ((x84 << 63) & UINT64_C(0xffffffffffffffff)));
-  x116 = ((x84 >>
+  x116 = ((x84 >> 1) | ((x86 << 63) & UINT64_C(0xffffffffffffffff)));
+  x117 = ((x86 >> 1) | ((x88 << 63) & UINT64_C(0xffffffffffffffff)));
+  x118 = ((x88 & UINT6
