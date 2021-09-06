2bace7e835793e85ddbd65eaf10c0e32d82e816c
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-09-06 17:36:53 +0000

    going home

--- pasta_fp.c
+++ pasta_fp.c
@@ -2010,4 +2010,6 @@ void fiat_pasta_fp_divstep(uint64_t* out1, uint64_t out2[5], uint64_t out3[5], u
   fiat_pasta_fp_subborrowx_u64(&x104, &x105, x103, x96, UINT64_C(0x224698fc094cf91b));
   fiat_pasta_fp_subborrowx_u64(&x106, &x107, x105, x98, 0x0);
   fiat_pasta_fp_subborrowx_u64(&x108, &x109, x107, x100, UINT64_C(0x4000000000000000));
-  fiat_past
+  fiat_pasta_fp_subborrowx_u64(&x110, &x111, x109, x101, 0x0);
+  fiat_pasta_fp_addcarryx_u64(&x112, &x113, 0x0, x6, 0x1);
+  x114 = 
