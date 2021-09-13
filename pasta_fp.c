ab4f795f724df0a5f36f2059abbf68d4b9df8158
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-09-13 10:44:17 +0000

    5.1.3

--- pasta_fp.c
+++ pasta_fp.c
@@ -2083,4 +2083,13 @@ bool fiat_pasta_fp_equals(const uint64_t x[4], const uint64_t y[4]) {
 
   uint64_t x_minus_y_nonzero;
   fiat_pasta_fp_nonzero(&x_minus_y_nonzero, x_minus_y);
-  if (x_minu
+  if (x_minus_y_nonzero) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+bool fiat_pasta_fp_equals_zero(const uint64_t x[4]) {
+  uint64_t x_nonzero;
+  fia
