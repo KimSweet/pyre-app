b6c302a44204bcd4a3ce5f02c561c16630e08cac
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-10-11 21:31:18 +0000

    poll

--- pasta_fq.c
+++ pasta_fq.c
@@ -283,4 +283,11 @@ void fiat_pasta_fq_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
   uint64_t x141;
   uint64_t x142;
   uint64_t x143;
-  fiat_past
+  fiat_pasta_fq_uint1 x144;
+  uint64_t x145;
+  fiat_pasta_fq_uint1 x146;
+  uint64_t x147;
+  fiat_pasta_fq_uint1 x148;
+  uint64_t x149;
+  uint64_t x150;
+  fiat_pasta_fq_uint1 x15
