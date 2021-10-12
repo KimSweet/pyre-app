a79c3e1aef5dcf9282a5627f58cd55ade0129ae1
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-10-12 07:08:46 +0000

    Initial commit, pushing theme over to github

--- pasta_fq.c
+++ pasta_fq.c
@@ -306,4 +306,10 @@ void fiat_pasta_fq_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
   uint64_t x164;
   uint64_t x165;
   uint64_t x166;
-  
+  uint64_t x167;
+  uint64_t x168;
+  fiat_pasta_fq_uint1 x169;
+  uint64_t x170;
+  uint64_t x171;
+  fiat_pasta_fq_uint1 x172;
+  uint64_t x17
