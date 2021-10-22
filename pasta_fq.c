95aaf0f3656833d45bcc9c169582e041ac7163f9
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-10-22 05:27:46 +0000

    Maven artifacts for Messaging version 1.1.7-SNAPSHOT

--- pasta_fq.c
+++ pasta_fq.c
@@ -414,4 +414,6 @@ void fiat_pasta_fq_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
   fiat_pasta_fq_mulx_u64(&x141, &x142, x3, (arg2[0]));
   fiat_pasta_fq_addcarryx_u64(&x143, &x144, 0x0, x142, x139);
   fiat_pasta_fq_addcarryx_u64(&x145, &x146, x144, x140, x137);
- 
+  fiat_pasta_fq_addcarryx_u64(&x147, &x148, x146, x138, x135);
+  x149 = (x148 + x136);
+  fiat_pasta_fq_addcarryx_u64(&x150, &x151, 0x0, x126,
