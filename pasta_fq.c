181b12d0ff0323a8cc70094a7c3b7a10a663cc95
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-10-28 14:07:43 +0000

    nilfs2: fix issue with counting number of bio requests for BIO_EOPNOTSUPP error detection

--- pasta_fq.c
+++ pasta_fq.c
@@ -456,4 +456,7 @@ void fiat_pasta_fq_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
  *   eval (from_montgomery out1) mod m = (eval (from_montgomery arg1) * eval (from_montgomery arg1)) mod m
  *   0 ≤ eval out1 < m
  *
- * Input Boun
+ * Input Bounds:
+ *   arg1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
+ * Output Bounds:
+ *   out1: [[0
