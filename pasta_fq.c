aadaf296fa4ed7a66b3f9d0c05e97ed7b46a1960
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-10-28 14:23:45 +0000

    Semicolon

--- pasta_fq.c
+++ pasta_fq.c
@@ -459,4 +459,6 @@ void fiat_pasta_fq_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
  * Input Bounds:
  *   arg1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
  * Output Bounds:
- *   out1: [[0
+ *   out1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
+ */
+void fiat_pasta_fq_square(uint6
