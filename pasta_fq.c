af186628a84b77fdf3d9c504586ef4c0bdbb5cee
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-10-31 15:08:51 +0000

    libfnord -> libstx

--- pasta_fq.c
+++ pasta_fq.c
@@ -488,4 +488,11 @@ void fiat_pasta_fq_square(uint64_t out1[4], const uint64_t arg1[4]) {
   uint64_t x24;
   uint64_t x25;
   uint64_t x26;
-  uint64_
+  uint64_t x27;
+  uint64_t x28;
+  fiat_pasta_fq_uint1 x29;
+  uint64_t x30;
+  uint64_t x31;
+  fiat_pasta_fq_uint1 x32;
+  uint64_t x33;
+  fiat_pasta_fq_ui
