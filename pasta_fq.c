fb10ce614a2d93d1d3f462f03a4a6655fa656806
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-10-31 17:07:42 +0000

    Add some screenshots

--- pasta_fq.c
+++ pasta_fq.c
@@ -503,4 +503,14 @@ void fiat_pasta_fq_square(uint64_t out1[4], const uint64_t arg1[4]) {
   uint64_t x39;
   fiat_pasta_fq_uint1 x40;
   uint64_t x41;
-  uint64_
+  uint64_t x42;
+  uint64_t x43;
+  uint64_t x44;
+  uint64_t x45;
+  uint64_t x46;
+  uint64_t x47;
+  uint64_t x48;
+  uint64_t x49;
+  fiat_pasta_fq_uint1 x50;
+  uint64_t x51;
+  fiat_pa
