8a8cbf768400a67deff41b5eaab90e1d209df360
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-11-01 09:41:05 +0000

    maintain ChangeLog

--- pasta_fq.c
+++ pasta_fq.c
@@ -513,4 +513,11 @@ void fiat_pasta_fq_square(uint64_t out1[4], const uint64_t arg1[4]) {
   uint64_t x49;
   fiat_pasta_fq_uint1 x50;
   uint64_t x51;
-  fiat_pa
+  fiat_pasta_fq_uint1 x52;
+  uint64_t x53;
+  fiat_pasta_fq_uint1 x54;
+  uint64_t x55;
+  uint64_t x56;
+  fiat_pasta_fq_uint1 x57;
+  uint64_t x58;
+  fi
