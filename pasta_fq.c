2d594d4b447431188913696e8f648f791365c084
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-11-08 16:55:06 +0000

    rpc_pipefs: allow rpc_purge_list to take a NULL waitq pointer

--- pasta_fq.c
+++ pasta_fq.c
@@ -666,4 +666,7 @@ void fiat_pasta_fq_square(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fq_mulx_u64(&x9, &x10, x4, (arg1[1]));
   fiat_pasta_fq_mulx_u64(&x11, &x12, x4, (arg1[0]));
   fiat_pasta_fq_addcarryx_u64(&x13, &x14, 0x0, x12, x9);
-  fiat_pasta_fq_addcarryx_u64(&x
+  fiat_pasta_fq_addcarryx_u64(&x15, &x16, x14, x10, x7);
+  fiat_pasta_fq_addcarryx_u64(&x17, &x18, x16, x8, x5);
+  x19 = (x18 + x6);
+  fiat_pasta_fq_mulx_u64(&x20, &x21, x11, UINT64_C(0x8c46
