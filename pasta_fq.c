4ad7440264a2261c6615874ac722f2152ca67482
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-11-08 06:31:13 +0000

    loplugin:loopvartoosmall

--- pasta_fq.c
+++ pasta_fq.c
@@ -646,4 +646,10 @@ void fiat_pasta_fq_square(uint64_t out1[4], const uint64_t arg1[4]) {
   uint64_t x182;
   fiat_pasta_fq_uint1 x183;
   uint64_t x184;
-  fiat_pasta_f
+  fiat_pasta_fq_uint1 x185;
+  uint64_t x186;
+  fiat_pasta_fq_uint1 x187;
+  uint64_t x188;
+  fiat_pasta_fq_uint1 x189;
+  uint64_t x190;
+  fiat_pasta_fq_ui
