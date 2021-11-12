efd0a490df6e80f0eb0c77595c815b7bf7ffb397
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-11-12 11:37:48 +0000

    Spelling fix

--- pasta_fq.c
+++ pasta_fq.c
@@ -695,4 +695,6 @@ void fiat_pasta_fq_square(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fq_addcarryx_u64(&x64, &x65, x63, x40, x55);
   fiat_pasta_fq_mulx_u64(&x66, &x67, x56, UINT64_C(0x8c46eb20ffffffff));
   fiat_pasta_fq_mulx_u64(&x68, &x69, x66, UINT64_C(0x4000000000000000));
-  fiat_pasta_fq_m
+  fiat_pasta_fq_mulx_u64(&x70, &x71, x66, UINT64_C(0x224698fc0994a8dd));
+  fiat_pasta_fq_mulx_u64(&x72, &x73, x66, UINT64_C(0x8c46eb2100000001));
+  fiat_pasta_fq_addca
