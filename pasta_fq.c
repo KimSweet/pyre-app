d00a440515a5748fd98f927ff0f4d8523b9a100d
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-11-16 18:07:46 +0000

    refactor styles

--- pasta_fq.c
+++ pasta_fq.c
@@ -733,4 +733,6 @@ void fiat_pasta_fq_square(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fq_mulx_u64(&x135, &x136, x3, (arg1[3]));
   fiat_pasta_fq_mulx_u64(&x137, &x138, x3, (arg1[2]));
   fiat_pasta_fq_mulx_u64(&x139, &x140, x3, (arg1[1]));
-  
+  fiat_pasta_fq_mulx_u64(&x141, &x142, x3, (arg1[0]));
+  fiat_pasta_fq_addcarryx_u64(&x143, &x144, 0x0, x142, x139);
+  fiat_pasta_fq_addcarryx_u64(&x145, &x146, x144, x140,
