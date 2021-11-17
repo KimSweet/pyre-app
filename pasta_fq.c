b060567e8a9187c73cf9a55ee897437a9efb2342
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-11-17 13:03:57 +0000

    GPL license

--- pasta_fq.c
+++ pasta_fq.c
@@ -738,4 +738,7 @@ void fiat_pasta_fq_square(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fq_addcarryx_u64(&x145, &x146, x144, x140, x137);
   fiat_pasta_fq_addcarryx_u64(&x147, &x148, x146, x138, x135);
   x149 = (x148 + x136);
-  fiat_pasta_fq_addcarryx_u64(&x150, &x151, 0x0, x12
+  fiat_pasta_fq_addcarryx_u64(&x150, &x151, 0x0, x126, x141);
+  fiat_pasta_fq_addcarryx_u64(&x152, &x153, x151, x128, x143);
+  fiat_pasta_fq_addcarryx_u64(&x154, &x155, x153, x130, x145);
+  fiat_pa
