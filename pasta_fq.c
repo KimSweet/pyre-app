35c4d9b22977ac8f0ef399faddc31dc974f00c21
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-11-28 07:51:20 +0000

    Updated contributors.

--- pasta_fq.c
+++ pasta_fq.c
@@ -837,4 +837,5 @@ void fiat_pasta_fq_add(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
  *   0 ≤ eval out1 < m
  *
  * Input Bounds:
- *   arg1: [[
+ *   arg1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
+ *   arg2: [[0x0 ~> 0xffff
