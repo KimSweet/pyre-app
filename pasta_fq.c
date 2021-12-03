ae05043d2855a751e4b7434f9692d5f6c14b3dcf
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-12-03 15:19:55 +0000

    Fix install command

--- pasta_fq.c
+++ pasta_fq.c
@@ -884,4 +884,6 @@ void fiat_pasta_fq_sub(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
  *   0 ≤ eval out1 < m
  *
  * Input Bounds:
- *   arg1: [[0x0 ~> 0xffffffffffffffff],
+ *   arg1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
+ * Output Bounds:
+ *   out1: [[0x0 ~> 0xfffffffffffff
