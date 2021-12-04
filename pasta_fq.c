dcd0150df040c2a00052bd0930e2f2e62a893f4f
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-12-04 14:14:05 +0000

    Add to TODO

--- pasta_fq.c
+++ pasta_fq.c
@@ -886,4 +886,6 @@ void fiat_pasta_fq_sub(uint64_t out1[4], const uint64_t arg1[4], const uint64_t 
  * Input Bounds:
  *   arg1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
  * Output Bounds:
- *   out1: [[0x0 ~> 0xfffffffffffff
+ *   out1: [[0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff], [0x0 ~> 0xffffffffffffffff]]
+ */
+void fiat_pasta_fq_opp(uint64_t out1[4], const uint64_
