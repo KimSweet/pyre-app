4f06ee4b6006bd087ba416e2ddb784bce494ab0e
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-12-13 05:39:24 +0000

    Some code clean up

--- pasta_fq.c
+++ pasta_fq.c
@@ -949,4 +949,13 @@ void fiat_pasta_fq_from_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   uint64_t x12;
   fiat_pasta_fq_uint1 x13;
   uint64_t x14;
-  fiat_pasta_fq
+  fiat_pasta_fq_uint1 x15;
+  uint64_t x16;
+  fiat_pasta_fq_uint1 x17;
+  uint64_t x18;
+  uint64_t x19;
+  uint64_t x20;
+  uint64_t x21;
+  uint64_t x22;
+  uint64_t x23;
+  u
