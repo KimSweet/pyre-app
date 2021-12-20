841c63748297481f8f50abfabfceebda0b8526c3
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-12-20 12:15:29 +0000

    update it translation

--- pasta_fq.c
+++ pasta_fq.c
@@ -1046,4 +1046,6 @@ void fiat_pasta_fq_from_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fq_mulx_u64(&x20, &x21, x18, UINT64_C(0x4000000000000000));
   fiat_pasta_fq_mulx_u64(&x22, &x23, x18, UINT64_C(0x224698fc0994a8dd));
   fiat_pasta_fq_mulx_u64(&x24, &x25, x18, UINT64_C(0x8c46eb2100000001));
-  fiat
+  fiat_pasta_fq_addcarryx_u64(&x26, &x27, 0x0, x25, x22);
+  fiat_pasta_fq_addcarryx_u64(&x28, &x29, 0x0, x16, x24);
+  fiat_pasta_fq_addcarryx_u64(&x30, &x
