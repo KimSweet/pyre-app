98df280c54e9315b5ed97b87ac394718faefc54e
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-12-26 17:27:25 +0000

    add scripts

--- pasta_fq.c
+++ pasta_fq.c
@@ -1110,4 +1110,14 @@ void fiat_pasta_fq_to_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   uint64_t x3;
   uint64_t x4;
   uint64_t x5;
-  uint64_
+  uint64_t x6;
+  uint64_t x7;
+  uint64_t x8;
+  uint64_t x9;
+  uint64_t x10;
+  uint64_t x11;
+  uint64_t x12;
+  uint64_t x13;
+  fiat_pasta_fq_uint1 x14;
+  uint64_t x15;
+  f
