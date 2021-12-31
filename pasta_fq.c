50263638752e406c8b10051055ca3eaf787a45c6
KimSweet <101127850+KimSweet@users.noreply.github.com>
2021-12-31 15:48:51 +0000

    new about page

--- pasta_fq.c
+++ pasta_fq.c
@@ -1241,4 +1241,12 @@ void fiat_pasta_fq_to_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fq_uint1 x134;
   uint64_t x135;
   fiat_pasta_fq_uint1 x136;
-  uint
+  uint64_t x137;
+  fiat_pasta_fq_uint1 x138;
+  uint64_t x139;
+  uint64_t x140;
+  uint64_t x141;
+  uint64_t x142;
+  uint64_t x143;
+  uint64_t x144;
+  uint64_t 
