a7a7dc795d7b4842519a5070d83f02c1b67a4276
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-01-02 15:01:45 +0000

    fix dependency version

--- pasta_fq.c
+++ pasta_fq.c
@@ -1287,4 +1287,6 @@ void fiat_pasta_fq_to_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fq_addcarryx_u64(&x13, &x14, 0x0, x12, x9);
   fiat_pasta_fq_addcarryx_u64(&x15, &x16, x14, x10, x7);
   fiat_pasta_fq_addcarryx_u64(&x17, &x18, x16, x8, x5);
- 
+  fiat_pasta_fq_mulx_u64(&x19, &x20, x11, UINT64_C(0x8c46eb20ffffffff));
+  fiat_pasta_fq_mulx_u64(&x21, &x22, x19, UINT64_C(0x4000000000000000));
+  fiat_pasta_fq_
