c7d705d2a351e0b87c3ab54bb24593c6dbbb0363
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-01-08 22:54:38 +0000

    Fixed indentation error

--- pasta_fq.c
+++ pasta_fq.c
@@ -1335,4 +1335,6 @@ void fiat_pasta_fq_to_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fq_addcarryx_u64(&x109, &x110, 0x0, x91, x105);
   fiat_pasta_fq_addcarryx_u64(&x111, &x112, x110, x93, x107);
   fiat_pasta_fq_addcarryx_u64(&x113, &x114, x112, x95, (x108 + x104));
-  fiat_pasta_fq_add
+  fiat_pasta_fq_addcarryx_u64(&x115, &x116, x114, x97, x101);
+  fiat_pasta_fq_mulx_u64(&x117, &x118, x3, UINT64_C(0x96d41af7ccfdaa9));
+  fiat_pasta_fq_mulx_u64(&x119, &x120, x
