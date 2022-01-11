203ded53ffa48278e6f6aa6498d02caa8d0cd25e
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-01-11 23:10:34 +0000

    Commit build changes [ci skip]

--- pasta_fq.c
+++ pasta_fq.c
@@ -1361,4 +1361,7 @@ void fiat_pasta_fq_to_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fq_subborrowx_u64(&x160, &x161, x159, x153, UINT64_C(0x224698fc0994a8dd));
   fiat_pasta_fq_subborrowx_u64(&x162, &x163, x161, x155, 0x0);
   fiat_pasta_fq_subborrowx_u64(&x164, &x165, x163, x157, UINT64_C(0x4000000000000000));
-  fiat_pasta_fq_su
+  fiat_pasta_fq_subborrowx_u64(&x166, &x167, x165, 0x0, 0x0);
+  fiat_pasta_fq_cmovznz_u64(&x168, x167, x158, x151);
+  fiat_pasta_fq_cmovznz_u64(&x169, x167, x160, x153);
+  fiat_pas
