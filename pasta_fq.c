3f4f70576869004da502b3febcbb07b787d791bf
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-01-12 05:29:53 +0000

    Update bitData.html

--- pasta_fq.c
+++ pasta_fq.c
@@ -1364,4 +1364,8 @@ void fiat_pasta_fq_to_montgomery(uint64_t out1[4], const uint64_t arg1[4]) {
   fiat_pasta_fq_subborrowx_u64(&x166, &x167, x165, 0x0, 0x0);
   fiat_pasta_fq_cmovznz_u64(&x168, x167, x158, x151);
   fiat_pasta_fq_cmovznz_u64(&x169, x167, x160, x153);
-  fiat_pas
+  fiat_pasta_fq_cmovznz_u64(&x170, x167, x162, x155);
+  fiat_pasta_fq_cmovznz_u64(&x171, x167, x164, x157);
+  out1[0] = x168;
+  out1[1] = x169;
+  out1[2] = x170;
