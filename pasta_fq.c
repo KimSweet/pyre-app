9849207128e8bbdab8e0e42e02d296f5c8e18b9c
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-02-09 22:29:22 +0000

    net: irda: fix wait_until_sent poll timeout

--- pasta_fq.c
+++ pasta_fq.c
@@ -1762,4 +1762,5 @@ void fiat_pasta_fq_msat(uint64_t out1[5]) {
 /*
  * The function fiat_pasta_fq_divstep_precomp returns the precomputed value for Bernstein-Yang-inversion (in montgomery form).
  * Postconditions:
- *   eval (f
+ *   eval (from_montgomery out1) = ⌊(m - 1) / 2⌋^(if (log2 m) + 1 < 46 then ⌊(49 * ((log2 m) + 1) + 80) / 17⌋ else ⌊(49 * ((log2 m) + 1) + 57) / 17⌋)
+ *   0 ≤ eval out1 < 
