015c7de96c1c4da2fcf31a631c01229cbb7a84a5
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-03-05 19:14:17 +0000

    hw_random: make buffer usable in scatterlist.

--- pasta_fq.c
+++ pasta_fq.c
@@ -1943,4 +1943,6 @@ void fiat_pasta_fq_divstep(uint64_t* out1, uint64_t out2[5], uint64_t out3[5], u
   fiat_pasta_fq_cmovznz_u64(&x11, x3, (arg2[4]), (arg3[4]));
   fiat_pasta_fq_addcarryx_u64(&x12, &x13, 0x0, 0x1, (~(arg2[0])));
   fiat_pasta_fq_addcarryx_u64(&x14, &x15, x13, 0x0, (~(arg2[1])));
-  fiat_pasta_
+  fiat_pasta_fq_addcarryx_u64(&x16, &x17, x15, 0x0, (~(arg2[2])));
+  fiat_pasta_fq_addcarryx_u64(&x18, &x19, x17, 0x0, (~(arg2[3])));
+  fiat_pasta_fq_addcarryx_u64(&x20, &x
