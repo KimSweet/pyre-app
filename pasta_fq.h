068de6922439d48a1f0a70412ec1a1dd150b46bc
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-05-23 16:06:58 +0000

    Update library

--- pasta_fq.h
+++ pasta_fq.h
@@ -1,3 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
-#i
+#include <stdbool.h>
+
+void fiat_pasta_fq_set_one(uint64_t out1[4]);
+void fiat_pasta_fq_add(uint64_t out1[4], const uint64_t arg1[4], const uint64_t arg2[4]);
+void f
