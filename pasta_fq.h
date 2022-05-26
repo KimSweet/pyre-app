f895568955b6de699121472a0a83f3329a4c879f
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-05-26 10:56:06 +0000

    isofs: Refuse RW mount of the filesystem instead of making it RO

--- pasta_fq.h
+++ pasta_fq.h
@@ -4,4 +4,5 @@
 
 void fiat_pasta_fq_set_one(uint64_t out1[4]);
 void fiat_pasta_fq_add(uint64_t out1[4], const uint64_t arg1[4], const uint64_t arg2[4]);
-void f
+void fiat_pasta_fq_sub(uint64_t out1[4], const uint64_t arg1[4], const uint64_t arg2[4]);
+void fiat_pasta_fq_mul(uint64_t out1[4], const uint64_t arg1[4], const uint
