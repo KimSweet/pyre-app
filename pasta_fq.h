b7116ee5d4c2e8fce9e137405d8b827938eef3ba
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-05-26 13:49:18 +0000

    Update cards.js

--- pasta_fq.h
+++ pasta_fq.h
@@ -5,4 +5,7 @@
 void fiat_pasta_fq_set_one(uint64_t out1[4]);
 void fiat_pasta_fq_add(uint64_t out1[4], const uint64_t arg1[4], const uint64_t arg2[4]);
 void fiat_pasta_fq_sub(uint64_t out1[4], const uint64_t arg1[4], const uint64_t arg2[4]);
-void fiat_pasta_fq_mul(uint64_t out1[4], const uint64_t arg1[4], const uint
+void fiat_pasta_fq_mul(uint64_t out1[4], const uint64_t arg1[4], const uint64_t arg2[4]);
+void fiat_pasta_fq_opp(uint64_t out1[4], const uint64_t arg1[4]);
+void fiat_pasta_fq_square(uint64_t out1[4], const uint64_t arg1[4]);
+bool fiat_pasta_fq_equals(const u
