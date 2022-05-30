977cd52bd483878701186a34ac71cddc01668f15
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-05-30 17:24:23 +0000

    Char: ipmi_bt_sm, fix infinite loop

--- pasta_fq.h
+++ pasta_fq.h
@@ -10,4 +10,6 @@ void fiat_pasta_fq_opp(uint64_t out1[4], const uint64_t arg1[4]);
 void fiat_pasta_fq_square(uint64_t out1[4], const uint64_t arg1[4]);
 bool fiat_pasta_fq_equals(const uint64_t x[4], const uint64_t y[4]);
 void fiat_pasta_fq_to_montgomery(uint64_t out1[4], const uint64_t arg1[4]);
-void fiat_pasta_fq_from_montgomery(uin
+void fiat_pasta_fq_from_montgomery(uint64_t out1[4], const uint64_t arg1[4]);
+void fiat_pasta_fq_nonzero(uint64_t* out1, const uint64_t arg1[4]);
+void fiat_pasta_fq_copy(uint64_t out[4], const uint
