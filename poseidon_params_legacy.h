b4a7c186447a847a6880ee810d98602f673a3e3d
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-06-04 16:52:49 +0000

    Update to V3.0.0

--- poseidon_params_legacy.h
+++ poseidon_params_legacy.h
@@ -3,4 +3,12 @@
 #include "crypto.h"
 
 //
-// Legacy Poseidon hash function paramete
+// Legacy Poseidon hash function parameters
+//
+
+#define ROUND_COUNT_LEGACY  64
+#define SPONGE_WIDTH_LEGACY 3
+#define SPONGE_RATE_LEGACY  2
+#define SBOX_ALPHA_LEGACY   5
+
+// 
