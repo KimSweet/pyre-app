5407f24262fb132c34205f30a78b5d9f0706ca24
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-06-06 16:08:42 +0000

    removed trailing comma

--- poseidon_params_legacy.h
+++ poseidon_params_legacy.h
@@ -15,4 +15,6 @@
 static const Field round_keys_legacy[ROUND_COUNT_LEGACY][SPONGE_WIDTH_LEGACY] =
 {
     {
-        {0xd2425a07cfec91d, 0x6130240fd42
+        {0xd2425a07cfec91d, 0x6130240fd42af5be, 0x3fb56f00f649325, 0x107d26d6fefb125f},
+        {0x1dcedf2d0ebcb628, 0x2381dfa5face2460, 0x24e92a6d36d75404, 0xce8a325b8f74c91},
+        {0x5df2ca
