a5a0bac402d684c1b6b840ad314c104e6ecc934c
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-08-18 19:25:07 +0000

    Bump to v0.2

--- poseidon_params_legacy.h
+++ poseidon_params_legacy.h
@@ -160,4 +160,6 @@ static const Field round_keys_legacy[ROUND_COUNT_LEGACY][SPONGE_WIDTH_LEGACY] =
         {0x2267900dcf1e0fe4, 0xaf0c04b7861398c6, 0x55b20fd2619336f9, 0x3729ba618d213b74}
     },
     {
-        {0x7bccad65b5fd53a3, 0x275fd70abb
+        {0x7bccad65b5fd53a3, 0x275fd70abbee0824, 0x6fce1c43407f5ddd, 0x381402c3966b0d15},
+        {0xd4e90f5f5d1b1215, 0x7abfb980598b6f39, 0x6cc413a7353c523b, 0x3af9e5c6dbcecf69},
+        {0xfc86b3533d
