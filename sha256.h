afb10cab71c7f84f649b8dd874fb0929b764926c
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-12-11 19:07:06 +0000

    Update from Forestry.io Anna Banyik updated _data/changelog.yml

--- sha256.h
+++ sha256.h
@@ -13,4 +13,7 @@
 #include <stddef.h>
 
 /****************************** MACROS ******************************/
-#define 
+#define SHA256_BLOCK_SIZE 32            // SHA256 outputs a 32 byte digest
+
+/**************************** DATA TYPES ****************************/
+typedef unsigned char BYTE;             
