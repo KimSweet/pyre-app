ea8a057e874cf3da8dc1a0119c274ddd0527ccec
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-12-11 13:01:22 +0000

    Add missing return statement.

--- sha256.h
+++ sha256.h
@@ -3,4 +3,10 @@
 * Author:     Brad Conte (brad AT bradconte.com)
 * Copyright:
 * Disclaimer: This code is presented "as is" without any guarantees.
-* Details:    Defines the API for the corresponding SHA1 implementatio
+* Details:    Defines the API for the corresponding SHA1 implementation.
+*********************************************************************/
+
+#ifndef SHA256_H
+#define SHA256_H
+
+/*************************** HEA
