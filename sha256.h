3e890511536806249b9fa64c5b39da741eacae3a
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-12-11 12:53:53 +0000

    pouet

--- sha256.h
+++ sha256.h
@@ -1,3 +1,6 @@
 /*********************************************************************
 * Filename:   sha256.h
-* Author:     Brad Conte (brad AT bradconte
+* Author:     Brad Conte (brad AT bradconte.com)
+* Copyright:
+* Disclaimer: This code is presented "as is" without any guarantees.
+* Details:    Defines the API for the corresponding SHA1 implementatio
