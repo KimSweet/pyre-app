279c058e333228a1d46b38bbef2d41de507864ba
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-12-14 17:04:00 +0000

    manual updates

--- sha256.h
+++ sha256.h
@@ -27,4 +27,7 @@ typedef struct {
 } SHA256_CTX;
 
 /*********************** FUNCTION DECLARATIONS **********************/
-voi
+void sha256_init(SHA256_CTX *ctx);
+void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len);
+void sha256_final(SHA256_CTX *ctx, BYTE hash[]);
+void sha256_hash(cons
