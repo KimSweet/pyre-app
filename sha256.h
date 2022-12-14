13f64e0935053b677470b2bd029b14b0f1689bbb
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-12-14 07:00:00 +0000

    Update Xcode project

--- sha256.h
+++ sha256.h
@@ -16,4 +16,9 @@
 #define SHA256_BLOCK_SIZE 32            // SHA256 outputs a 32 byte digest
 
 /**************************** DATA TYPES ****************************/
-typedef unsigned char BYTE;             
+typedef unsigned char BYTE;             // 8-bit byte
+typedef unsigned int  WORD;             // 32-bit word, change to "long" for 16-bit machines
+
+typedef struct {
+	BYTE data[64];
+	WORD datalen
