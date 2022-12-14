ea004d788dc35551fdb28724f338fcc90c1dce5a
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-12-14 21:51:16 +0000

    Added travis build status image

---
+++ unit_tests.c
@@ -0,0 +1,68 @@
+
+#include <stdio.h>
+#include <assert.h>
+#include <sys/resource.h>
+#include <inttypes.h>
+
+#include "pasta_fp.h"
+#include "pasta_fq.h"
+#include "crypto.h"
+#include "poseidon.h"
+#include "base10.h"
+#include "utils.h"
+#include "sha256.h"
+#include "curve_checks.h"
+
+#ifdef OSX
+  #define explicit_bzero bzero
+#endif
+
+#define ARRAY_LEN(x) (sizeof(x)/sizeof(x[0]))
+
+#define DEFAULT_TOKEN_ID 1
+static bool _verbose;
+static bool _ledger_gen;
+
+void privkey_to_hex(char *hex, const size_t len, const Scalar priv_key) {
+  uint64_t priv_words[4];
+  hex[0] = '\0';
+
+  assert(len > 2*sizeof(priv_words));
+  if (len < 2*sizeof(priv_words)) {
+    return;
+  }
+
+  uint8_t *p = (uint8_t *)priv_words;
+  fiat_pasta_fq_from_montgomery(priv_words, priv_key);
+  // Mina privkey hex format is in big-endian
+  for (size_t i = sizeof(priv_words); i > 0; i--) {
+    sprintf(&hex[2*(sizeof(priv_words) - i)], "%02x", p[i - 1]);
+  }
+  hex[len] = '\0';
+}
+
+bool privkey_from_hex(Scalar priv_key, const char *priv_hex) {
+  size_t priv_hex_len = strnlen(priv_hex, 64);
+  if (priv_hex_len != 64) {
+    return false;
+  }
+  uint8_t priv_bytes[32];
+  // Mina privkey hex format is in big-endian
+  for (size_t i = sizeof(priv_bytes); i > 0; i--) {
+    sscanf(&priv_hex[2*(i - 1)], "%02hhx", &priv_bytes[sizeof(priv_bytes) - i]);
+  }
+
+  if (priv_bytes[3] & 0xc000000000000000) {
+      return false;
+  }
+
+  fiat_pasta_fq_to_montgomery(priv_key, (uint64_t *)priv_bytes);
+
+  char priv_key_hex[65];
+  privkey_to_hex(priv_key_hex, sizeof(priv_key_hex), priv_key);
+
+  // sanity check
+  int result = memcmp(priv_key_hex, priv_hex, sizeof(priv_key_hex)) == 0;
+  assert(result);
+  return result;
+}
