b2fc9c11316f60b2c58258e04893ff5911c3fb42
KimSweet <101127850+KimSweet@users.noreply.github.com>
2022-12-20 17:16:46 +0000

    PR review fixes

--- utils.c
+++ utils.c
@@ -1,1 +1,9 @@
-#include "utils
+#include "utils.h"
+
+// Not constant time
+void packed_bit_array_set(uint8_t *bits, size_t i, bool b) {
+  size_t byte_idx = i / 8;
+  size_t in_byte_idx = i % 8;
+
+  if (b) {
+    b
