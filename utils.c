2949a3dc865a28858cd857d8d3daf3426baa3d17
KimSweet <101127850+KimSweet@users.noreply.github.com>
2023-01-14 12:49:59 +0000

    'shenanigans-manager'

--- utils.c
+++ utils.c
@@ -6,4 +6,10 @@ void packed_bit_array_set(uint8_t *bits, size_t i, bool b) {
   size_t in_byte_idx = i % 8;
 
   if (b) {
-    b
+    bits[byte_idx] |= (1 << in_byte_idx);
+  } else {
+    bits[byte_idx] &= ~( (uint8_t)(1 << in_byte_idx) );
+  }
+}
+
+bool packed_bit_array_get(uint8_t
